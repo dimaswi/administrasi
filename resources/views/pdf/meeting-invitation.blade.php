765d70efe730077eab759c21b5b2e73689ec6584
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-11-12 13:39:48 +0700

    Update meeting-invitation.blade.php

--- resources/views/pdf/meeting-invitation.blade.php
+++ resources/views/pdf/meeting-invitation.blade.php
@@ -175,9 +175,9 @@
                     <br>
                 @endif
                 
-                <strong><u>{{ $meeting_leader->name }}</u></strong>
-                @if($meeting_leader->nip)
-                    <br><span style="font-size: 10pt;">NIP. {{ $meeting_leader->nip }}</span>
+                <strong><u>{{ $leader>name }}</u></strong>
+                @if($leader>nip)
+                    <br><span style="font-size: 10pt;">NIP. {{ $leader>nip }}</span>
                 @endif
             </td>
         </tr>
