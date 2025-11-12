58f0676071429595939367727bf3e83595fe3f6b
Dimas Wisnu Wirawan <101412302+dimaswi@users.noreply.github.com>
2025-11-12 13:53:50 +0700

    Update meeting-invitation.blade.php

--- resources/views/pdf/meeting-invitation.blade.php
+++ resources/views/pdf/meeting-invitation.blade.php
@@ -166,7 +166,7 @@
             <td style="text-align: center; padding-top: 10px; padding-bottom: 5px;">
                 @php
                     $moderator = $meeting->participants->where('role', 'moderator')->first();
-                    <!-- $leader = $moderator ? $moderator->user : $meeting->organizer;  -->
+                    $leader = $moderator ? $moderator->user : $meeting->organizer; 
                 @endphp
                 
                 <!-- QR Code Signature Certificate -->
@@ -175,10 +175,10 @@
                     <br>
                 @endif
                 
-                <!-- <strong><u>{{ $meeting->organizer->name }}</u></strong>
-                @if($meeting->organizer->nip)
-                    <br><span style="font-size: 10pt;">NIP. {{ $meeting->organizer->nip }}</span>
-                @endif -->
+                <strong><u>{{ $leader->name }}</u></strong>
+                @if($leader->nip)
+                    <br><span style="font-size: 10pt;">NIP. {{ $leader->nip }}</span>
+                @endif
             </td>
         </tr>
     </table>
